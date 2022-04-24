b39af8dab04a71a3b19fe944ceef9055f001f6dc
LovePlanes <33170989+GeetanshGautam0@users.noreply.github.com>
2022-04-23 20:11:55 -0700

    Fixed hash bug

---
+++ qa_apps_admin_tools.py
@@ -0,0 +1,34 @@
+from threading import Thread
+import tkinter as tk, sys
+from typing import *
+
+
+script_name = "APP_AT"
+
+
+class _UI(Thread):
+    def __init__(self, root, ic, ds, **kwargs):
+        super().__init__()
+        Thread.__init__(self)
+
+        self.root, self.ic, self.ds, self.kwargs = root, ic, ds, kwargs
+
+        self.start()
+        self.root.mainloop()
+
+    def close(self):
+        sys.stdout.write("at - _UI.close")
+        self.ic.shell = self.ds
+        self.ic.shell_ready = False
+
+        self.root.after(0, self.root.quit)
+
+    def run(self):
+        self.root.protocol('WM_DELETE_WINDOW', self.close)
+
+
+def RunApp(instance_class: object, default_shell: Union[tk.Tk, tk.Toplevel], **kwargs):
+    ui_root = tk.Tk()
+    cls = _UI(ui_root, ic=instance_class, ds=default_shell, **kwargs)
+
+    return ui_root
